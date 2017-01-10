404d6c04278e7e328aaa81e81d3e82ba6a373839
Kadir Yasar <kyasar.mail@mail.com>
2017-01-10 21:04:35 +0300

    Spring Boot, Security(OAuth2), JPA, Hibernate, MongoDB, Swagger, JUnit dependencies are included, and project created.

---
+++ src/main/java/com/cherchy/markod/MapisApplication.java
@@ -0,0 +1,12 @@
+package com.cherchy.markod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapisApplication.class, args);
+	}
+}
