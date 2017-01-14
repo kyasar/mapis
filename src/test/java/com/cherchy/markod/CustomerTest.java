b12854fe010167afb3557af0fb16c18626efe954
Kadir Yasar <kyasar.mail@mail.com>
2017-01-14 16:05:04 +0300

    MArket and Customer base tests are done.

--- src/test/java/com/cherchy/markod/CustomerTest.java
+++ src/test/java/com/cherchy/markod/CustomerTest.java
@@ -1,13 +1,9 @@
 package com.cherchy.markod;
 
-import com.cherchy.markod.model.Campaign;
+import com.cherchy.markod.model.Customer;
 import com.cherchy.markod.model.Market;
-import com.cherchy.markod.model.Price;
-import com.cherchy.markod.model.Product;
-import com.cherchy.markod.service.CampaignService;
+import com.cherchy.markod.service.CustomerService;
 import com.cherchy.markod.service.MarketService;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,49 +13,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class MarketTest {
+public class CustomerTest {
 
     @Autowired
     private MongoTemplate mongoTemplate;
 
     @Autowired
-    private MarketService marketService;
+    private CustomerService customerService;
 
     @Test
     public void t0_setUp() {
-        mongoTemplate.remove(new Query(), "markets");
+        mongoTemplate.remove(new Query(), "customers");
     }
 
     @Test
     public void t1_insertMarkets() {
 
-        List<Market> markets = Arrays.asList(
-            new Market("Altunbilekler", "address 1", new Point(39.894177, 32.801571)),
-            new Market("Migros", "address 2", new Point(39.893387, 32.79788)),
-            new Market("Cagdas", "address 3", new Point(39.892827, 32.799082)),
-            new Market("Kiler", "address 4", new Point(39.895, 32.797837)),
-            new Market("Makro", "address 5", new Point(39.889732, 32.800627))
+        List<Customer> customers = Arrays.asList(
+            new Customer("Kadir", "Yasar", "ayasar@gmail.com", "12345"),
+            new Customer("Kadir2", "Yasar", "kyasar@gmail.com", "111"),
+            new Customer("Kadir", "Yasar", "kadir.mail@gmail.com", "112233")
         );
 
-        for (Market m : markets) {
-            Market created = marketService.create(m);
+        for (Customer c : customers) {
+            Customer created = customerService.create(c);
             if (created != null) {
-                System.out.println("Market created: " + created.getId());
+                System.out.println("Customer created: " + created.getId());
             } else {
-                System.err.println("Market NOT created: " + m.getId());
+                System.err.println("Customer NOT created: " + c.getId());
             }
         }
     }
@@ -69,8 +58,8 @@ public class MarketTest {
     @Test
     public void t2_getMarkets() {
 
-        List<Market> markets = marketService.findAll();
-        for (Market m : markets) {
+        List<Customer> customers = customerService.findAll();
+        for (Customer m : customers) {
             System.out.println(m.getId() + " " + m.getName());
             marketId = m.getId();
         }
@@ -78,7 +67,7 @@ public class MarketTest {
 
     @Test
     public void t3_getMarket() {
-        Market m = marketService.findOne(marketId);
+        Customer m = customerService.findOne(marketId);
         if (m != null) {
             System.out.println(m.getId() + " " + m.getName());
         }
